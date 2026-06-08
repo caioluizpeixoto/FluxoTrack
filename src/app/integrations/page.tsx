578f80cb35ef8b7cc9ae538a639698abfb945084
Caio luiz <caioluispeixotos@gmail.com>
2026-06-07 23:30:48 -0300

    first commit

--- src/app/integrations/page.tsx
+++ src/app/integrations/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import IntegrationsContent from "./IntegrationsContent";
 import { Loader2 } from "lucide-react";
 
+export const dynamic = "force-dynamic";
+
 export default function IntegrationsPage() {
   return (
     <Suspense
